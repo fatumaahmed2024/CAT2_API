4bbe7788bb7a078bdb8ec1077925444c9f20b690
fatumaahmed2024 <Fatuma.Mohamed@strathmore.edu>
2024-11-27 10:42:02 +0300

    customer_model

--- ecommerce/customer/models.py
+++ ecommerce/customer/models.py
@@ -5,6 +5,7 @@ class Customer(models.Model):
     first_name=models.CharField(max_length=50)
     last_name=models.CharField(max_length=50)
     email=models.EmailField(unique=TRUE)
-
-    def__str___(self):
-        return f"{self.first_name} {self.last_name}"
+    
+  def __str__(self):
+    return self.first_name +" " +self.last_name
+        
