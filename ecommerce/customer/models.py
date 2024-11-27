fc5a4e259111cdfb78816af2d43d86d8d2150278
fatumaahmed2024 <Fatuma.Mohamed@strathmore.edu>
2024-11-27 13:07:11 +0300

    orders_model

--- ecommerce/customer/models.py
+++ ecommerce/customer/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Customer(models.Model):
     first_name=models.CharField(max_length=50)
     last_name=models.CharField(max_length=50)
-    email=models.EmailField(unique=TRUE)
+    email=models.EmailField(unique=True)
     
-  def __str__(self):
+   def __str__(self):
     return self.first_name +" " +self.last_name
         
--- ecommerce/orders/models.py
+++ ecommerce/orders/models.py
@@ -2,4 +2,10 @@ from django.db import models
 from customer.models import Customer
 
 # Create your models here.
-class Customer
+class Order(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE, related_name='orders')
+    order_date=models.DateTimeField(auto_now_add=True)
+    order_total=models.DecimalField(max_digits=10,decimal_places=2)
+    
+    def __str__(self):
+        return self.customer.first_name +" " +self.customer.last_name +" "+str(self.order_total)
